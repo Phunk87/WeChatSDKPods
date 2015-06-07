ec96abb838c1606e5a087d57a0f9e1bf4bdf4c84
0dayZh <0day.zh@gmail.com>
2015-06-08 01:28:43 +0800

    Update example

--- Example/Pods/Target Support Files/Pods-WeChatSDKPods_Tests/Pods-WeChatSDKPods_Tests-environment.h
+++ Example/Pods/Target Support Files/Pods-WeChatSDKPods_Tests/Pods-WeChatSDKPods_Tests-environment.h
@@ -9,12 +9,12 @@
 // WeChatSDKPods
 #define COCOAPODS_POD_AVAILABLE_WeChatSDKPods
 #define COCOAPODS_VERSION_MAJOR_WeChatSDKPods 1
-#define COCOAPODS_VERSION_MINOR_WeChatSDKPods 5
+#define COCOAPODS_VERSION_MINOR_WeChatSDKPods 0
 #define COCOAPODS_VERSION_PATCH_WeChatSDKPods 0
 
 // WeChatSDKPods/precompiled
 #define COCOAPODS_POD_AVAILABLE_WeChatSDKPods_precompiled
 #define COCOAPODS_VERSION_MAJOR_WeChatSDKPods_precompiled 1
-#define COCOAPODS_VERSION_MINOR_WeChatSDKPods_precompiled 5
+#define COCOAPODS_VERSION_MINOR_WeChatSDKPods_precompiled 0
 #define COCOAPODS_VERSION_PATCH_WeChatSDKPods_precompiled 0
 
--- WeChatSDKPods.podspec
+++ WeChatSDKPods.podspec
@@ -9,7 +9,7 @@
 
 Pod::Spec.new do |s|
   s.name             = "WeChatSDKPods"
-  s.version          = "1.5.0"
+  s.version          = "1.0.0"
   s.summary          = "WeChatSDK's Pods"
   s.homepage         = "https://github.com/0dayZh/WeChatSDKPods"
   # s.screenshots     = "www.example.com/screenshots_1", "www.example.com/screenshots_2"
